c02eafe1e76a6347ea3ac09935a18b6c7e75e29c
Miguel Bittar <miguelbittar29@gmail.com>
2025-08-17 10:54:55 -0300

    Add main application entry point

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println("Starting calculator...");
+        ConsoleInterface console = new ConsoleInterface();
+        console.runLoop();
+        System.out.println("Calculator closed.");
     }
 }
